fcb2875c31d54ca10e8cee70402ecd337370eafe
lirta <indalirta@gmail.com>
2020-03-09 04:12:30 +0700

    soal edit

--- pages/soal/hapus.php
+++ pages/soal/hapus.php
@@ -1,8 +1,7 @@
 <?php
 include "../../coneksi/config.php";
 
-$queri="DELETE FROM lowongan WHERE lowongan_id='$_GET[id]';";
-$queri .="DELETE FROM detail_lowongan WHERE lowongan_id='$_GET[id]'";
+$queri="DELETE FROM soal WHERE id='$_GET[id]';";
 mysqli_multi_query($koneksi,$queri);
 mysqli_close($koneksi);
 header('location:view.php');
