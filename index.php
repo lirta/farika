0d1d3466bb3e9898215ec5ce007c29ef9d981362
lirta <indalirta@gmail.com>
2020-03-09 05:49:53 +0700

    fron end

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Djaya Hydrolic</title>
+    <title>Bee - Free Bootstrap 4 Template by Colorlib</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
@@ -29,115 +28,249 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <?php include 'config.php';
-          include 'navbar.php'; ?>
-	<?php include 'menu.php'; ?>
+    <div class="bg-top navbar-light">
+      <div class="container">
+        <div class="row no-gutters d-flex align-items-center align-items-stretch">
+          <div class="col-md-4 d-flex align-items-center py-4">
+            <a class="navbar-brand" href="index.html"><span class="flaticon-bee mr-1"></span>Djaya Hydrolic</a>
+          </div>
+          <div class="col-lg-8 d-block">
+            <div class="row d-flex">
+              <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+                <div class="text d-flex align-items-center">
+                  <span>customer@farikariau.com</span>
+                </div>
+              </div>
+              <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+                <div class="text d-flex align-items-center">
+                  <span>(0761) 571655, 08127589884, 081276505571</span>
+                </div>
+              </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container d-flex align-items-center">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="oi oi-menu"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active"><a href="index.html" class="nav-link pl-0">Branda</a></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">Tentang Kami</a></li>
+            <li class="nav-item"><a href="project.html" class="nav-link">Produk</a></li>
+            <li class="nav-item"><a href="blog.html" class="nav-link">Lowongan Pekerjaan</a></li>
+            <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <!-- END nav -->
     
     <section class="home-slider owl-carousel">
-      <div class="slider-item" style="background-image:url(<?php echo "admin/foto/$home[slide_a]"; ?>);" data-stellar-background-ratio="0.5">
+      <div class="slider-item" style="background-image:url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+        
       </div>
 
-      <div class="slider-item" style="background-image:url(<?php echo "admin/foto/$home[slide_b]"; ?>);" data-stellar-background-ratio="0.5">
+      <div class="slider-item" style="background-image:url(images/bg_2.jpg);" data-stellar-background-ratio="0.5">
+       
     </section>
 
-
-    <section class="ftco-section ftco-no-pt ftco-margin-top">
-    	<div class="container">
-    		<div class="row justify-content-center mb-5 pb-2">
-    			<div class="col-md-8 text-center wrap-about py-5 ftco-animate">
-        		<div class="heading-section mb-5">
-              <span class="subheading">Profile</span>
-	            <h2 class="mb-4"><?php echo "$home[nama_perusahaan]"; ?></h2>
-    	        </div>
-            		<div class="">
-                 <?php $querif = "SELECT * FROM profile";
-                  $hasilf = mysqli_query($koneksi,$querif);
-                  $profile=mysqli_fetch_assoc($hasilf);  ?> 
-    					<p class="mb-5"><?php echo "$profile[profile]"; ?></p>
-    							
-    					</div>
-				  </div>
-    		</div>
-    	</div>
-    </section>
-	
-	<section class="ftco-section ftco-no-pt ftco-margin-top">
-		<div class="container">
-			<div class="row justify-content-center mb-5 pb-2">
-        <div class="col-md-8 text-center heading-section ftco-animate">
-          	<span class="subheading">Fasilitas</span>
-            <h2 class="mb-4">fASILITAS YANG KAMI MILIKI</h2>
-           <p>Dalam membantu dan memperlancar kegiatan usaha kami, untuk sekarang ini kami memiliki mesin diantaranya :</p>
-        </div>
-      </div>	
-			<div class="row">
-				 <?php
-			          $queria = "SELECT * FROM fasilitas ";
-			          $hasila = mysqli_query($koneksi,$queria);
-			          $no=1;
-			          while($koloma=mysqli_fetch_assoc($hasila))
-			          { ?>
-				<div class="col-md-6 col-lg-3 ftco-animate">
-					<div class="staff">
-						<div class="img-wrap d-flex align-items-stretch">
-							<div class="img align-self-stretch" style="background-image: url(<?php echo "admin/fasilitas/$koloma[foto_f]"; ?>);"></div>
-						</div>
-						<div class="text pt-3 text-center">
-							<h3><?php echo "$koloma[nama]"; ?></h3>
-						</div>
-					</div>
-				</div>
-				<?php $no=$no+1;
-                      }
-                  ?>
-				</div>
-			</div>
-		</section>
-
-		<section class="ftco-section ">
-			<div class="container-fluid p-0">
-				<div class="row no-gutters justify-content-center mb-5 pb-2">
-          <div class="col-md-6 text-center heading-section ftco-animate">
-          	<span class="subheading">Projects</span>
-            <h2 class="mb-4">Projects Yang pernah kami Kerjakan</h2>
+    <section class="ftco-services ftco-no-pt">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-2">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+            <span class="subheading">Profile</span>
+            <h2 class="mb-4">Siapa Kami</h2>
+            <p>PT. Farika Riau Perkasa merupakan perusahaan yang bergerak dalam bidang penyediaan readymix concrete (beton readymix), produk precast beton dan rental equipment untuk proyek-proyek pembangunan dan industri umum.</p>
           </div>
-              <div class="row no-gutters">
-               <?php
-      	          $queriee = "SELECT * FROM projec ";
-      	          $hasilee = mysqli_query($koneksi,$queriee);
-      	          $no=1;
-      	          while($kolomee=mysqli_fetch_assoc($hasilee))
-      	          { ?>
-          		
-          			<div class="col-md-6 col-lg-3 ftco-animate">
-          				<div class="project">
-      	    				<img src="<?php echo "admin/project/$kolomee[foto_p]"; ?>" class="img-fluid" alt="Colorlib Template">
-      	    				<div class="text">
-      	    					<h3><a href="project.html"><?php echo "$kolomee[nama]"; ?></a></h3>
-      	    				</div>
-      	    				<a href="<?php echo "admin/project/$kolomee[foto_p]"; ?>" class="icon image-popup d-flex justify-content-center align-items-center">
-      	    					<span class="icon-expand"></span>
-      	    				</a>
-          				</div>
-          			</div>
-          			<?php $no=$no+1;
-                            }
-                        ?>
-    			
-    		      </div>
-    	   </div>
-       </div>
-		</section>
+        </div>
+        <div class="row">
+          <div class="col-md-6 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block ">
+              <div class="icon d-flex justify-content-center align-items-center">
+                <span class="flaticon-hook"></span>
+              </div>
+              <div class="media-body p-2 mt-6">
+                <div class="media block-6 d-block text-center">
+                <h3 class="heading">VISI</h3> </div>
+                <ul>
+                  <li>Menjadikan PT. FARIKA RIAU PERKASA sebagai perusahaan bersakala Nasional yang memproduksi beton readymix & precast yang berkualitas.</li>
+                  <li>Pelayanan yang lebih baik.</li>
+                  <li>Selalu melakukan inovasi dan improvisasi yang berkesinambungan agar dapat meningkatkan kinerja dengan efektif dan efisien.</li>
+                </ul>
+                
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-6 d-flex services align-self-stretch p-4 ftco-animate">
+            <div class="media block-6 d-block">
+              <div class="icon d-flex justify-content-center align-items-center">
+                <span class="flaticon-skyline"></span>
+              </div>
+              <div class="media-body p-2 mt-6">
+                <div class="media block-6 d-block text-center">
+                <h3 class="heading">MISI</h3> </div>
+                <ul>
+                  <li>Menyediakan beton readymix & precast dengan mutu terjamin.</li>
+                  <li>Material berkualitas.</li>
+                  <li>Volume tepat.</li>
+                  <li>Harga bersaing.</li>
+                  <li>Layanan yang sigap serta luwes.</li>
+                </ul>
+              </div>
+            </div>    
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-2">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+            <span class="subheading">Produk</span>
+            <h2 class="mb-4">Produk Yang Pernah Kami Tangani</h2>
+          </div>
+        </div>  
+        <div class="row">
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="staff">
+              <div class="img-wrap d-flex align-items-stretch">
+                <div class="img align-self-stretch" style="background-image: url(images/team-1.jpg);"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="staff">
+              <div class="img-wrap d-flex align-items-stretch">
+                <div class="img align-self-stretch" style="background-image: url(images/team-2.jpg);"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="staff">
+              <div class="img-wrap d-flex align-items-stretch">
+                <div class="img align-self-stretch" style="background-image: url(images/team-3.jpg);"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
-		
-		<?php include 'whatsapp.php';?>
-		<?php include 'footer.php'; ?>
+    <section class="ftco-section ftco-no-pt ftco-no-pb">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters justify-content-center mb-5 pb-2">
+          <div class="col-md-6 text-center heading-section ftco-animate">
+            <span class="subheading">PORTOFOLIO</span>
+            <h2 class="mb-4">PORTOFOLIO</h2>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-1.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Gedung</a></h3>
+              </div>
+              <a href="images/work-1.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-2.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Mall</a></h3>
+              </div>
+              <a href="images/work-2.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-3.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Hotel</a></h3>
+              </div>
+              <a href="images/work-3.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-4.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Jalan</a></h3>
+              </div>
+              <a href="images/work-4.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-5.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Pabrik</a></h3>
+              </div>
+              <a href="images/work-5.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ftco-animate">
+            <div class="project">
+              <img src="images/work-6.jpg" class="img-fluid" alt="Colorlib Template">
+              <div class="text">
+                <h3><a href="project.html">Dermaga</a></h3>
+              </div>
+              <a href="images/work-6.jpg" class="icon image-popup d-flex justify-content-center align-items-center">
+                <span class="icon-expand"></span>
+              </a>
+            </div>
+        </div>
+      </div>
+    </section>
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">KANTOR</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Alamat       : PT. FARIKA RIAU PERKASA</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Email Kantor : farikariauperkasa@ymail.com, customer@farikariau.com</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Painting</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Interior Design</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Exterior Design</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
     
   
 
   <!-- loader -->
-  
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
   <script src="js/jquery.min.js"></script>
