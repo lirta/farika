cf546eafcceb5e120fd6208a5947ff4e93400eb5
lirta <indalirta@gmail.com>
2020-03-14 03:09:39 +0700

    admin baru

--- farika/kariawan/hrd/sidebar.php
+++ farika/kariawan/hrd/sidebar.php
@@ -6,7 +6,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -28,12 +28,32 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="calon_pelamar/view.php" class="nav-link">
+            <a href="profile.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
-              <p>Calon Pelamar</p>
+              <p>Profil</p>
             </a>
           </li>
-
+          <li class="nav-item">
+            <a href="../data_pelamar/view.php" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>Data Pelamar</p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>Data Lamaran<i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../data_lamaran/view.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Lamaran</p>
+                </a>
+              </li>
+            </ul>
+          </li>
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
@@ -45,7 +65,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="lowongan/view.php" class="nav-link">
+                <a href="../lowongan/view.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Kategori lowongan</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../lowongan/view.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>lowongan</p>
                 </a>
@@ -55,20 +81,20 @@
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-edit"></i>
-              <p>Soal<i class="fas fa-angle-left right"></i>
+              <p>Master Soal<i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="soal/view.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Soal</p>
-                </a>
-              </li>
                 <li class="nav-item">
-                <a href="kategori_soal/view.php" class="nav-link">
+                <a href="../kategori_soal/view.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Kategori Soal</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../soal/view.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Soal</p>
                 </a>
               </li>
             </ul>
--- farika/pages/login/proses_login.php
+++ farika/pages/login/proses_login.php
@@ -11,9 +11,18 @@
     session_start();
     $_SESSION['username']            = $r['username'];
     $_SESSION['password']            = $r['password'];
+    $_SESSION['foto']                = $r['kariawan_foto'];
+    $_SESSION['akses']               = $r['kariawan_jabatan'];
 
-    header('location:../../admin/index.php');
+    if ($_SESSION['akses'] == "ADMIN") {
+      header('location:../../kariawan/admin/index.php');
     mysqli_close($koneksi);
+    }elseif ($_SESSION['HRD'] == "HRD") {
+      header('location:../../kariawan/hrd/index.php');
+    mysqli_close($koneksi);
+    }
+
+    
   }
   else {
     header('location:login.php');
