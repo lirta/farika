ae2e90bb3c1388f8ca0434d2aa692fcc841062b8
lirta <indalirta@gmail.com>
2020-06-02 00:25:52 +0700

    perbaikan soal

--- farika/kariawan/soal/view.php
+++ farika/kariawan/soal/view.php
@@ -1,83 +1,96 @@
 <?php include "../../coneksi/config.php";
-if (!isset($_SESSION)) {session_start();}
-if (empty($_SESSION['username']) AND
-    empty($_SESSION['password']))
-    {include "login.php";}
-    else {
+if (!isset($_SESSION)) {
+  session_start();
+}
+if (
+  empty($_SESSION['username']) and
+  empty($_SESSION['password'])
+) {
+  include "login.php";
+} else {
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PT. Farika Riau Perkasa</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!DOCTYPE html>
+  <html>
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  <!-- Navbar -->
-  <?php include 'navbar.php'; include 'sidebar.php'; ?>
-  <!-- /.navbar -->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PT. Farika Riau Perkasa</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Main Sidebar Container -->
- 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  </head>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Data Soal</h1>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+  <body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+      <!-- Navbar -->
+      <?php include 'navbar.php';
+      include 'sidebar.php'; ?>
+      <!-- /.navbar -->
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-12">
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Data</h3> <br>
-              <a href='add.php' class='btn btn-primary'>TAMBAH DATA</a>
+      <!-- Main Sidebar Container -->
+
+
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <div class="container-fluid">
+            <div class="row mb-2">
+              <div class="col-sm-6">
+                <h1>Data Soal</h1>
+              </div>
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Soal</th>
-                  <th>Opsi A</th>
-                  <th>Opsi B</th>
-                  <th>Opsi C</th>
-                  <th>Opsi D</th>
-                  <th>Jawaban</th>
-                  <th>Aksi</th>
-                </tr>
-                </thead>
-                <tbody>
+          </div><!-- /.container-fluid -->
+        </section>
 
-                <?php 
-                $queri ="SELECT * FROM soal";
-                $hasil =mysqli_query($koneksi,$queri);
-                $no = 1;
-                while ($kolom=mysqli_fetch_assoc($hasil)) {
-                  echo "
+        <!-- Main content -->
+        <section class="content">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Data</h3> <br>
+                  <a href='add.php' class='btn btn-primary'>TAMBAH DATA</a>
+                </div>
+                <!-- /.card-header -->
+                <?php
+                $qk = mysqli_query($koneksi, "SELECT * FROM kategori_soal");
+                while ($kat = mysqli_fetch_assoc($qk)) {
+                ?>
+
+                  <div class="card-body">
+                    <h3>Soal <?php echo "$kat[nama]" ?></h3>
+                    <table id="example1" class="table table-bordered table-striped">
+                      <thead>
+                        <tr>
+                          <th>Soal</th>
+                          <th>Opsi A</th>
+                          <th>Opsi B</th>
+                          <th>Opsi C</th>
+                          <th>Opsi D</th>
+                          <th>Jawaban</th>
+                          <th>Aksi</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        <?php
+                        $queri = "SELECT * FROM soal WHERE kategori_soal_id='$kat[kategori_soal_id]'";
+                        $hasil = mysqli_query($koneksi, $queri);
+                        $no = 1;
+                        while ($kolom = mysqli_fetch_assoc($hasil)) {
+                          echo "
                     <tr>
                             <td>$kolom[soal]</td>
                             <td>$kolom[a]</td>
@@ -89,60 +102,63 @@ if (empty($_SESSION['username']) AND
                             <a href='edit.php?id=$kolom[id]' class='btn btn-primary'>Edit</td>
                         </tr>
                         ";
-                      
-                        $no=$no+1;
-                 } 
-                 ?>
-                
-                </tbody>
-              </table>
+
+                          $no = $no + 1;
+                        }
+                        ?>
+
+                      </tbody>
+                    </table>
+                  </div>
+                <?php } ?>
+
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
             </div>
-            <!-- /.card-body -->
+            <!-- /.col -->
           </div>
-          <!-- /.card -->
-        </div>
-        <!-- /.col -->
+          <!-- /.row -->
+        </section>
+        <!-- /.content -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-<?php include '../footer.php'; ?>
+      <!-- /.content-wrapper -->
+      <?php include '../footer.php'; ?>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables -->
-<script src="../../plugins/datatables/jquery.dataTables.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<!-- page script -->
-<script>
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-    });
-  });
-</script>
-</body>
-</html>
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables -->
+    <script src="../../plugins/datatables/jquery.dataTables.js"></script>
+    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
+    <!-- page script -->
+    <script>
+      $(function() {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false,
+        });
+      });
+    </script>
+  </body>
+
+  </html>
 <?php } ?>
